6e76f8942507badcc77cb596e6b773f12a49b8c9
anonymous <shyeyian@icloud.com>
2025-12-27 00:49:03 +0800

    update

--- install.py
+++ install.py
@@ -1,9 +1,12 @@
 import shutil
 import subprocess
 
+# Install package
 for project in ["cppmakelib", "cppmake"]:
-    subprocess.run(f"python -m build", shell=True, cwd=project, check=True)
-    subprocess.run(f"pip install .",   shell=True, cwd=project, check=True)
-    shutil.rmtree(f"{project}/build",              ignore_errors=True)
-    shutil.rmtree(f"{project}/dist",               ignore_errors=True)
-    shutil.rmtree(f"{project}/{project}.egg-info", ignore_errors=True)
+    try:
+        subprocess.run(f"python -m build", shell=True, cwd=project, check=True)
+        subprocess.run(f"pip install .",   shell=True, cwd=project, check=True)
+    finally:
+        shutil.rmtree(f"{project}/build",              ignore_errors=True)
+        shutil.rmtree(f"{project}/dist",               ignore_errors=True)
+        shutil.rmtree(f"{project}/{project}.egg-info", ignore_errors=True)
--- upload.py
+++ upload.py
@@ -2,17 +2,27 @@ import re
 import shutil
 import subprocess
 
+# Git pull
 subprocess.run(f"git pull", shell=True, check=False)
+
+# Increment version
 for project in ["cppmakelib", "cppmake"]:
     with open(f"{project}/pyproject.toml", 'r') as reader:
         pyproject = reader.read()
     pyproject = re.sub(r'^version = "(\d+)\.(\d+)\.(\d+)"$', lambda match: f'version = "{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}"', pyproject, flags=re.MULTILINE)
     with open(f"{project}/pyproject.toml", 'w') as writer:
         writer.write(pyproject)
-    subprocess.run(f"python -m build",                                                                     shell=True, cwd=project, check=True)
-    subprocess.run(f"twine upload dist/* --username __token__ --password {open("pypi-token.txt").read()}", shell=True, cwd=project, check=True)
-    shutil.rmtree(f"{project}/dist")
-    shutil.rmtree(f"{project}/{project}.egg-info")
+
+# Twine upload
+for project in ["cppmakelib", "cppmake"]:
+    try:
+        subprocess.run(f"python -m build",                                                                     shell=True, cwd=project, check=True)
+        subprocess.run(f"twine upload dist/* --username __token__ --password {open("pypi-token.txt").read()}", shell=True, cwd=project, check=True)
+    finally:
+        shutil.rmtree(f"{project}/dist")
+        shutil.rmtree(f"{project}/{project}.egg-info")
+
+# Git push
 subprocess.run(f"git add .",            shell=True, check=False)
 subprocess.run(f"git commit -m update", shell=True, check=False)
 subprocess.run(f"git push",             shell=True, check=False)
