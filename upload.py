901ab1d42677e09124ac3a7087a969c74c3e9b23
shyeyian <shyeyian@icloud.com>
2026-02-02 22:19:39 +0800

    update

--- upload.py
+++ upload.py
@@ -17,40 +17,34 @@ except ImportError:
 # Git pull
 subprocess.run(['git', 'pull'])
 
-# Update subprojects
-for project in ['cppmakelib', 'cppmake', 'cppmaked']:
-    # Advance version
-    with open(pathlib.path()/project/'pyproject.toml', 'r') as reader:
-        pyproject = reader.read()
-    pyproject = re.sub(
-        pattern=r'^version = "(\d+)\.(\d+)\.(\d+)"$', 
-        repl   =lambda match: f'version = "{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}"', 
-        string =pyproject, 
-        flags  =re.MULTILINE
+# Twine upload
+with open('pyproject.toml', 'r') as reader:
+    pyproject = reader.read()
+pyproject = re.sub(
+    pattern=r'^version = "(\d+)\.(\d+)\.(\d+)"$', 
+    repl   =lambda match: f'version = "{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}"', 
+    string =pyproject, 
+    flags  =re.MULTILINE
+)
+with open('pyproject.toml', 'w') as writer:
+    writer.write(pyproject)
+try:
+    subprocess.run(
+        args=[sys.executable, '-m', 'build'], 
+        check=True
     )
-    with open(pathlib.path()/project/'pyproject.toml', 'w') as writer:
-        writer.write(pyproject)
-
-    # Twine upload
-    try:
-        subprocess.run(
-            args=[sys.executable, '-m', 'build'], 
-            cwd=project, 
-            check=True
-        ),
-        subprocess.run(
-            args=[
-                sys.executable, '-m', 
-                'twine', 'upload', 'dist/*', 
-                '--username', '__token__', 
-                '--password', open('pypi-token.txt').read()
-            ], 
-            cwd=project, 
-            check=True
-        )
-    finally:
-        shutil.rmtree(pathlib.path()/project/'dist')
-        shutil.rmtree(pathlib.path()/project/f'{project}.egg-info')
+    subprocess.run(
+        args=[
+            sys.executable, '-m', 
+            'twine', 'upload', 'dist/*', 
+            '--username', '__token__', 
+            '--password', open('pypi-token.txt').read()
+        ], 
+        check=True
+    )
+finally:
+    shutil.rmtree('dist')
+    shutil.rmtree(pathlib.Path()/'src'/'cppmake.egg-info')
 
 # Git push
 subprocess.run(['git', 'add', '.'])
